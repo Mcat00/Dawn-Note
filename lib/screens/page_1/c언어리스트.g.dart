3c44515738055593b29d2dda000e9ce918af9689
Mcat00 <96373223+Mcat00@users.noreply.github.com>
2022-05-13 15:10:29 +0900

    'init'

---
+++ lib/screens/page_1/c언어리스트.g.dart
@@ -0,0 +1,394 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+//
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+
+import 'package:flutter/material.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+
+class C extends StatefulWidget {
+  const C({
+    Key? key,
+  }) : super(key: key);
+  @override
+  _C createState() => _C();
+}
+
+class _C extends State<C> {
+  _C();
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Stack(children: [
+        Positioned(
+          left: 75.0,
+          width: 185.0,
+          top: 500.0,
+          height: 22.0,
+          child: Container(
+              width: 185.000,
+              height: 22.000,
+              child: AutoSizeText(
+                'C언어 01',
+                style: TextStyle(
+                  fontFamily: 'JejuGothic',
+                  fontSize: 18,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
+              )),
+        ),
+        Positioned(
+          left: 325.0,
+          width: 185.0,
+          top: 500.0,
+          height: 22.0,
+          child: Container(
+              width: 185.000,
+              height: 22.000,
+              child: AutoSizeText(
+                'C언어 02',
+                style: TextStyle(
+                  fontFamily: 'JejuGothic',
+                  fontSize: 18,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
+              )),
+        ),
+        Positioned(
+          left: 580.0,
+          width: 185.0,
+          top: 500.0,
+          height: 22.0,
+          child: Container(
+              width: 185.000,
+              height: 22.000,
+              child: AutoSizeText(
+                'C언어 03',
+                style: TextStyle(
+                  fontFamily: 'JejuGothic',
+                  fontSize: 18,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
+              )),
+        ),
+        Positioned(
+          left: 70.0,
+          width: 195.0,
+          top: 235.0,
+          bottom: 707.0,
+          child: Image.asset(
+            'assets/images/kakaotalk202205031938466222.png',
+            package: 'dawn_note',
+            width: 195.000,
+            height: MediaQuery.of(context).size.height * 0.211,
+            fit: BoxFit.fitHeight,
+          ),
+        ),
+        Positioned(
+          left: 325.0,
+          width: 195.0,
+          top: 235.0,
+          height: 252.0,
+          child: Image.asset(
+            'assets/images/kakaotalk202205032013593072.png',
+            package: 'dawn_note',
+            width: 195.000,
+            height: 252.000,
+            fit: BoxFit.none,
+          ),
+        ),
+        Positioned(
+          left: 580.0,
+          width: 195.0,
+          top: 235.0,
+          height: 252.0,
+          child: Image.asset(
+            'assets/images/kakaotalk202205032021507122.png',
+            package: 'dawn_note',
+            width: 195.000,
+            height: 252.000,
+            fit: BoxFit.none,
+          ),
+        ),
+        Positioned(
+          left: 250.0,
+          width: 394.0,
+          top: 180.0,
+          height: 50.0,
+          child: Container(
+              width: 394.000,
+              height: 50.000,
+              decoration: BoxDecoration(),
+              child: Stack(children: [
+                Positioned(
+                  left: 0,
+                  width: 394.0,
+                  top: 10.0,
+                  height: 40.0,
+                  child: Container(
+                      width: 394.000,
+                      height: 40.000,
+                      child: AutoSizeText(
+                        '.../ 중간고사 /  C언어',
+                        style: TextStyle(
+                          fontFamily: 'JejuGothic',
+                          fontSize: 15,
+                          fontWeight: FontWeight.w400,
+                          letterSpacing: 0,
+                          color: Colors.white,
+                        ),
+                        textAlign: TextAlign.center,
+                      )),
+                ),
+                Positioned(
+                  left: 50.0,
+                  width: 31.644,
+                  top: 0,
+                  height: 30.0,
+                  child: Image.asset(
+                    'assets/images/image55.png',
+                    package: 'dawn_note',
+                    width: 31.644,
+                    height: 30.000,
+                    fit: BoxFit.none,
+                  ),
+                ),
+              ])),
+        ),
+        Positioned(
+          left: 760.0,
+          width: 47.0,
+          top: 30.0,
+          height: 47.0,
+          child: Image.asset(
+            'assets/images/icons8902.png',
+            package: 'dawn_note',
+            width: 47.000,
+            height: 47.000,
+            fit: BoxFit.none,
+          ),
+        ),
+        Positioned(
+          left: 760.0,
+          width: 47.0,
+          top: 30.0,
+          height: 47.0,
+          child: Image.asset(
+            'assets/images/icons8903.png',
+            package: 'dawn_note',
+            width: 47.000,
+            height: 47.000,
+            fit: BoxFit.none,
+          ),
+        ),
+        Positioned(
+          left: 700.0,
+          width: 90.0,
+          top: 1060.0,
+          height: 109.0,
+          child: Container(
+              width: 90.000,
+              height: 109.000,
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0x40000000),
+                    spreadRadius: 4,
+                    blurRadius: 4,
+                    offset: Offset(0, 4),
+                  ),
+                ],
+              ),
+              child: Stack(children: [
+                Positioned(
+                  left: 0,
+                  width: 90.0,
+                  top: 0,
+                  height: 90.0,
+                  child: Image.asset(
+                    'assets/images/ellipse2.png',
+                    package: 'dawn_note',
+                    width: 90.000,
+                    height: 90.000,
+                    fit: BoxFit.none,
+                  ),
+                ),
+                Positioned(
+                  left: 12.0,
+                  width: 66.0,
+                  top: 0,
+                  height: 109.0,
+                  child: Container(
+                      width: 66.000,
+                      height: 109.000,
+                      child: AutoSizeText(
+                        '+',
+                        style: TextStyle(
+                          fontFamily: 'JejuGothic',
+                          fontSize: 100,
+                          fontWeight: FontWeight.w400,
+                          letterSpacing: 0,
+                          color: Color(0xffffbcd1),
+                        ),
+                        textAlign: TextAlign.center,
+                      )),
+                ),
+              ])),
+        ),
+        Positioned(
+          left: 292.0,
+          width: 251.0,
+          top: 122.0,
+          height: 57.0,
+          child: Container(
+              width: 251.000,
+              height: 57.000,
+              decoration: BoxDecoration(),
+              child: Stack(children: [
+                Positioned(
+                  left: 85.0,
+                  width: 92.0,
+                  top: 5.0,
+                  height: 46.0,
+                  child: Container(
+                    width: 92.000,
+                    height: 46.000,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(30)),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 91.0,
+                  width: 37.0,
+                  top: 10.0,
+                  height: 37.0,
+                  child: Image.asset(
+                    'assets/images/ellipse1.png',
+                    package: 'dawn_note',
+                    width: 37.000,
+                    height: 37.000,
+                    fit: BoxFit.none,
+                  ),
+                ),
+                Positioned(
+                  left: 0,
+                  width: 53.0,
+                  top: 0,
+                  height: 57.0,
+                  child: Container(
+                      width: 53.000,
+                      height: 57.000,
+                      child: AutoSizeText(
+                        'Note',
+                        style: TextStyle(
+                          fontFamily: 'Javanese Text',
+                          fontSize: 25,
+                          fontWeight: FontWeight.w400,
+                          letterSpacing: 0,
+                          color: Colors.white,
+                        ),
+                        textAlign: TextAlign.center,
+                      )),
+                ),
+                Positioned(
+                  left: 211.0,
+                  width: 40.0,
+                  top: 0,
+                  height: 57.0,
+                  child: Container(
+                      width: 40.000,
+                      height: 57.000,
+                      child: AutoSizeText(
+                        'Tag',
+                        style: TextStyle(
+                          fontFamily: 'Javanese Text',
+                          fontSize: 25,
+                          fontWeight: FontWeight.w400,
+                          letterSpacing: 0,
+                          color: Colors.white,
+                        ),
+                        textAlign: TextAlign.center,
+                      )),
+                ),
+              ])),
+        ),
+        Positioned(
+          left: 28.0,
+          width: 50.0,
+          top: 35.0,
+          height: 41.0,
+          child: Image.asset(
+            'assets/images/icons8501.png',
+            package: 'dawn_note',
+            width: 50.000,
+            height: 41.000,
+            fit: BoxFit.none,
+          ),
+        ),
+        Positioned(
+          left: 267.0,
+          width: 300.0,
+          top: 23.0,
+          height: 91.0,
+          child: Container(
+              width: 300.000,
+              height: 91.000,
+              decoration: BoxDecoration(),
+              child: Stack(children: [
+                Positioned(
+                  left: 0,
+                  width: 46.0,
+                  top: 16.0,
+                  height: 37.0,
+                  child: Image.asset(
+                    'assets/images/icons8moonsymbol602.png',
+                    package: 'dawn_note',
+                    width: 46.000,
+                    height: 37.000,
+                    fit: BoxFit.none,
+                  ),
+                ),
+                Positioned(
+                  left: 53.0,
+                  width: 247.0,
+                  top: 0,
+                  height: 91.0,
+                  child: Container(
+                      width: 247.000,
+                      height: 91.000,
+                      child: AutoSizeText(
+                        'DAWN NOTE',
+                        style: TextStyle(
+                          fontFamily: 'Javanese Text',
+                          fontSize: 40,
+                          fontWeight: FontWeight.w400,
+                          letterSpacing: 0,
+                          color: Colors.white,
+                        ),
+                        textAlign: TextAlign.center,
+                      )),
+                ),
+              ])),
+        ),
+      ]),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
---
+++ lib/screens/page_1/tag.g.dart
@@ -0,0 +1,374 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+//
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+
+import 'package:flutter/material.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+
+class Tag extends StatefulWidget {
+  const Tag({
+    Key? key,
+  }) : super(key: key);
+  @override
+  _Tag createState() => _Tag();
+}
+
+class _Tag extends State<Tag> {
+  _Tag();
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Stack(children: [
+        Positioned(
+          left: 75.0,
+          width: 185.0,
+          top: 430.0,
+          height: 22.0,
+          child: Container(
+              width: 185.000,
+              height: 22.000,
+              child: AutoSizeText(
+                '#RedCircle',
+                style: TextStyle(
+                  fontFamily: 'JejuGothic',
+                  fontSize: 18,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
+              )),
+        ),
+        Positioned(
+          left: 325.0,
+          width: 185.0,
+          top: 430.0,
+          height: 22.0,
+          child: Container(
+              width: 185.000,
+              height: 22.000,
+              child: AutoSizeText(
+                '#YellowHighlight ',
+                style: TextStyle(
+                  fontFamily: 'JejuGothic',
+                  fontSize: 18,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
+              )),
+        ),
+        Positioned(
+          left: 70.0,
+          width: 188.0,
+          top: 235.0,
+          height: 188.0,
+          child: Image.asset(
+            'assets/images/icons85001.png',
+            package: 'dawn_note',
+            width: 188.000,
+            height: 188.000,
+            fit: BoxFit.none,
+          ),
+        ),
+        Positioned(
+          left: 78.0,
+          width: 168.0,
+          top: 329.0,
+          height: 39.0,
+          child: Image.asset(
+            'assets/images/2022051134302.png',
+            package: 'dawn_note',
+            width: 168.000,
+            height: 39.000,
+            fit: BoxFit.none,
+          ),
+        ),
+        Positioned(
+          left: 325.0,
+          width: 188.0,
+          top: 235.0,
+          height: 188.0,
+          child: Image.asset(
+            'assets/images/icons85002.png',
+            package: 'dawn_note',
+            width: 188.000,
+            height: 188.000,
+            fit: BoxFit.none,
+          ),
+        ),
+        Positioned(
+          left: 336.0,
+          width: 167.0,
+          top: 314.0,
+          height: 68.0,
+          child: Image.asset(
+            'assets/images/2022051134547.png',
+            package: 'dawn_note',
+            width: 167.000,
+            height: 68.000,
+            fit: BoxFit.none,
+          ),
+        ),
+        Positioned(
+          left: 292.0,
+          width: 251.0,
+          top: 122.0,
+          height: 57.0,
+          child: Container(
+              width: 251.000,
+              height: 57.000,
+              decoration: BoxDecoration(),
+              child: Stack(children: [
+                Positioned(
+                  left: 85.0,
+                  width: 92.0,
+                  top: 5.0,
+                  height: 46.0,
+                  child: Container(
+                    width: 92.000,
+                    height: 46.000,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(30)),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 133.0,
+                  width: 37.0,
+                  top: 10.0,
+                  height: 37.0,
+                  child: Image.asset(
+                    'assets/images/ellipse1.png',
+                    package: 'dawn_note',
+                    width: 37.000,
+                    height: 37.000,
+                    fit: BoxFit.none,
+                  ),
+                ),
+                Positioned(
+                  left: 0,
+                  width: 53.0,
+                  top: 0,
+                  height: 57.0,
+                  child: Container(
+                      width: 53.000,
+                      height: 57.000,
+                      child: AutoSizeText(
+                        'Note',
+                        style: TextStyle(
+                          fontFamily: 'Javanese Text',
+                          fontSize: 25,
+                          fontWeight: FontWeight.w400,
+                          letterSpacing: 0,
+                          color: Colors.white,
+                        ),
+                        textAlign: TextAlign.center,
+                      )),
+                ),
+                Positioned(
+                  left: 211.0,
+                  width: 40.0,
+                  top: 0,
+                  height: 57.0,
+                  child: Container(
+                      width: 40.000,
+                      height: 57.000,
+                      child: AutoSizeText(
+                        'Tag',
+                        style: TextStyle(
+                          fontFamily: 'Javanese Text',
+                          fontSize: 25,
+                          fontWeight: FontWeight.w400,
+                          letterSpacing: 0,
+                          color: Colors.white,
+                        ),
+                        textAlign: TextAlign.center,
+                      )),
+                ),
+              ])),
+        ),
+        Positioned(
+          left: 760.0,
+          width: 47.0,
+          top: 30.0,
+          height: 47.0,
+          child: Image.asset(
+            'assets/images/icons8902.png',
+            package: 'dawn_note',
+            width: 47.000,
+            height: 47.000,
+            fit: BoxFit.none,
+          ),
+        ),
+        Positioned(
+          left: 700.0,
+          width: 90.0,
+          top: 1060.0,
+          height: 109.0,
+          child: Container(
+              width: 90.000,
+              height: 109.000,
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0x40000000),
+                    spreadRadius: 4,
+                    blurRadius: 4,
+                    offset: Offset(0, 4),
+                  ),
+                ],
+              ),
+              child: Stack(children: [
+                Positioned(
+                  left: 0,
+                  width: 90.0,
+                  top: 0,
+                  height: 90.0,
+                  child: Image.asset(
+                    'assets/images/ellipse2.png',
+                    package: 'dawn_note',
+                    width: 90.000,
+                    height: 90.000,
+                    fit: BoxFit.none,
+                  ),
+                ),
+                Positioned(
+                  left: 12.0,
+                  width: 66.0,
+                  top: 0,
+                  height: 109.0,
+                  child: Container(
+                      width: 66.000,
+                      height: 109.000,
+                      child: AutoSizeText(
+                        '+',
+                        style: TextStyle(
+                          fontFamily: 'JejuGothic',
+                          fontSize: 100,
+                          fontWeight: FontWeight.w400,
+                          letterSpacing: 0,
+                          color: Color(0xffffbcd1),
+                        ),
+                        textAlign: TextAlign.center,
+                      )),
+                ),
+              ])),
+        ),
+        Positioned(
+          left: 250.0,
+          width: 394.0,
+          top: 180.0,
+          height: 50.0,
+          child: Container(
+              width: 394.000,
+              height: 50.000,
+              decoration: BoxDecoration(),
+              child: Stack(children: [
+                Positioned(
+                  left: 0,
+                  width: 394.0,
+                  top: 10.0,
+                  height: 40.0,
+                  child: Container(
+                      width: 394.000,
+                      height: 40.000,
+                      child: AutoSizeText(
+                        '.../ 중간고사 / 자료구조',
+                        style: TextStyle(
+                          fontFamily: 'JejuGothic',
+                          fontSize: 15,
+                          fontWeight: FontWeight.w400,
+                          letterSpacing: 0,
+                          color: Colors.white,
+                        ),
+                        textAlign: TextAlign.center,
+                      )),
+                ),
+                Positioned(
+                  left: 50.0,
+                  width: 31.644,
+                  top: 0,
+                  height: 30.0,
+                  child: Image.asset(
+                    'assets/images/image55.png',
+                    package: 'dawn_note',
+                    width: 31.644,
+                    height: 30.000,
+                    fit: BoxFit.none,
+                  ),
+                ),
+              ])),
+        ),
+        Positioned(
+          left: 28.0,
+          width: 50.0,
+          top: 35.0,
+          height: 41.0,
+          child: Image.asset(
+            'assets/images/icons8501.png',
+            package: 'dawn_note',
+            width: 50.000,
+            height: 41.000,
+            fit: BoxFit.none,
+          ),
+        ),
+        Positioned(
+          left: 267.0,
+          width: 300.0,
+          top: 23.0,
+          height: 91.0,
+          child: Container(
+              width: 300.000,
+              height: 91.000,
+              decoration: BoxDecoration(),
+              child: Stack(children: [
+                Positioned(
+                  left: 0,
+                  width: 46.0,
+                  top: 16.0,
+                  height: 37.0,
+                  child: Image.asset(
+                    'assets/images/icons8moonsymbol602.png',
+                    package: 'dawn_note',
+                    width: 46.000,
+                    height: 37.000,
+                    fit: BoxFit.none,
+                  ),
+                ),
+                Positioned(
+                  left: 53.0,
+                  width: 247.0,
+                  top: 0,
+                  height: 91.0,
+                  child: Container(
+                      width: 247.000,
+                      height: 91.000,
+                      child: AutoSizeText(
+                        'DAWN NOTE',
+                        style: TextStyle(
+                          fontFamily: 'Javanese Text',
+                          fontSize: 40,
+                          fontWeight: FontWeight.w400,
+                          letterSpacing: 0,
+                          color: Colors.white,
+                        ),
+                        textAlign: TextAlign.center,
+                      )),
+                ),
+              ])),
+        ),
+      ]),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
