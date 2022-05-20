dfecb3adb37c1a93211bec27193a0d436c38781b
SANGJUN MOON <96373223+Mcat00@users.noreply.github.com>
2022-05-20 14:13:26 +0900

    del

--- lib/widgets/page_1/arrow1.g.dart
+++
@@ -1,8 +0,0 @@
-// *********************************************************************************
-// PARABEAC-GENERATED CODE. DO NOT MODIFY.
-// 
-// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
-// *********************************************************************************
-    
-
-SvgPicture.asset('assets/images/arrow1.svg', package: 'dawn_note',width: 573.000,height: 5.000, fit: BoxFit.none,)
