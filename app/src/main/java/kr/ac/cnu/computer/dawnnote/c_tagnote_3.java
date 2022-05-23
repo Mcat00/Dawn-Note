9532e4fab6e04341ecb69892b96fe86d1b3223d9
yeonarom <96374824+yeonarom@users.noreply.github.com>
2022-05-23 17:04:12 +0900

    c_tagnote

--- app/src/main/java/kr/ac/cnu/computer/dawnnote/c_tagnote_3.java
+++ app/src/main/java/kr/ac/cnu/computer/dawnnote/c_tagnote_3.java
@@ -3,12 +3,12 @@ package kr.ac.cnu.computer.dawnnote;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class c_list extends AppCompatActivity {
+public class c_tagnote_3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.c_list);
+        setContentView(R.layout.c_tagnote_3);
 
     }
 }
